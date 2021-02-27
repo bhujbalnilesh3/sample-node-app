7d05e4c3feb8762b25236980ab59e10a8de8c678
bhujbalnilesh3 <bhujbalnilesh3@gmail.com>
2021-02-27 10:57:28 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.status(200).json({
+    status: 200, 
+    message:'Hello from Nilesh Bhujbal.!'
+  });
+});
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080.!');
+});
