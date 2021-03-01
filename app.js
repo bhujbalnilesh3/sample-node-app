cd785cd4f678d62b2b50f7f1f68599cef99f24d9
bhujbalnilesh <bhujbalnilesh3@gmail.com>
2021-03-01 20:17:19 +0530

    Added containerid logging

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-﻿var express = require('express');
-var app = express();
+﻿const express = require('express');
+const getId = require('docker-container-id');
+const app = express();
+
+async function getContainerId () {
+  return await getId();
+}
+
 app.get('/', function (req, res) {
   res.status(200).json({
     status: 200,
+    containerId: getContainerId(),
     applicationVersion: '1.0', 
     message:'Hello from Nilesh Bhujbal.!'
   });
